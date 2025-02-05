f898548f2ef9bb9d086902e31140329c99df5f80
Hitoki Horie <horie@gunjo.org>
2025-02-05 21:46:51 +0900

    rebrand

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -16,13 +16,6 @@ export default async function InventoryLayout({
       <main className="relative z-10 min-h-[100svh] flex items-center justify-center animate-fade animate-delay-500 animate-duration-[600ms]">
         {children}
       </main>
-      <Image
-        src="/images/loginBackground.svg"
-        alt="Numerous ouranos logos"
-        width={1000}
-        height={200}
-        className="z-0 fixed bottom-0 w-screen h-[50svh] object-cover animate-fade-up animate-delay-0 animate-duration-[900ms]"
-      />
     </>
   );
 }
