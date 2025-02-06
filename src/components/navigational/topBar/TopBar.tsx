fbcd1628b0dcd7f358c30867417e59ad9ef21efb
Hitoki Horie <horie@gunjo.org>
2025-02-06 13:46:23 +0900

    rename /dashboard /user

--- src/components/navigational/topBar/TopBar.tsx
+++ src/components/navigational/topBar/TopBar.tsx
@@ -17,7 +17,7 @@ export default function TopBar(props: Props) {
   return (
     <div className="bg-skin-base border-skin-base sticky top-0 z-[60] flex items-center justify-between border-b px-3 py-2.5 transition-all ease-linear md:hidden">
       <Link
-        href={`/${profile?.handle}`}
+        href={`/user/${profile?.handle}`}
         className="hover:brightness-90"
       >
         <Avatar
