b35cddd638c9645d8b0220b509acba3981860a86
Hitoki Horie <horie@gunjo.org>
2025-02-06 11:57:14 +0900

    move user/[handle] -> /[handle]

--- src/app/dashboard/user/[handle]/(post)/post/[id]/liked-by/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@/containers/Layout";
-
-export default function StarredByLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Layout>{children}</Layout>;
-}
