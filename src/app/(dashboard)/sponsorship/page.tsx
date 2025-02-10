9733a3287750ee05c01a6cd2d6b81122d79810c2
Hitoki Horie <horie@gunjo.org>
2025-02-11 00:39:29 +0900

    Update page.tsx

--- src/app/(dashboard)/sponsorship/page.tsx
+++ src/app/(dashboard)/sponsorship/page.tsx
@@ -20,7 +20,7 @@ export default function FeedLayout({
           Become a Sponsor
         </h2>
         <Link
-          href="https://donate.stripe.com/">
+          href="https://donate.stripe.com/"
           className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
         >
           <BiLogoStripe className="text-skin-icon-base text-xl" />
