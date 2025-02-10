a76283a4ca2e944c3abd3f78184d1cf9b4eef1f8
Hitoki Horie <horie@gunjo.org>
2025-02-11 01:00:24 +0900

    Update page.tsx

--- src/app/(dashboard)/sponsorship/page.tsx
+++ src/app/(dashboard)/sponsorship/page.tsx
@@ -16,29 +16,24 @@ export default function Page() {
         <h2 className="text-skin-base mx-3 mb-2 text-2xl font-semibold md:mx-0">
           Sponsorship
         </h2>
-        <section>
-          <h3 className="text-skin-base mx-3 mb-2 text-xl font-semibold md:mx-0">
-            Become a Sponsor
-          </h3>
-          <div className="flex flex-col">
-            <Link
-              href="https://donate.stripe.com/"
-              target="_blank"
-              className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
-            >
-              <BiLogoStripe className="text-skin-icon-base text-xl" />
-              Manage Sponsorship
-            </Link>
-            <Link
-              href="https://foundation.gunjo.org/help/commerce-disclosure"
-              target="_blank"
-              className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
-            >
-              <TbScale className="text-skin-icon-base text-xl" />
-              Commerce Disclosure
-            </Link>
-          </div>
-        </section>
+        <div className="flex flex-col">
+          <Link
+            href="https://donate.stripe.com/"
+            target="_blank"
+            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
+          >
+            <BiLogoStripe className="text-skin-icon-base text-xl" />
+            Manage Sponsorship
+          </Link>
+          <Link
+            href="https://foundation.gunjo.org/help/commerce-disclosure"
+            target="_blank"
+            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
+          >
+            <TbScale className="text-skin-icon-base text-xl" />
+            Commerce Disclosure
+          </Link>
+        </div>
       </section>
     </Layout>
   );
