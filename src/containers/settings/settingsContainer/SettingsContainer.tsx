ef93f98bc78fae3be95f9263b0f941f8e2e09fed
Hitoki Horie <horie@gunjo.org>
2025-02-11 01:31:59 +0900

    edit sponsorship

--- src/containers/settings/settingsContainer/SettingsContainer.tsx
+++ src/containers/settings/settingsContainer/SettingsContainer.tsx
@@ -152,6 +152,29 @@ export default async function SettingsContainer() {
       </section>
       <section>
         <h3 className="text-skin-base mx-3 mb-2 text-xl font-semibold md:mx-0">
+          Manage Sponsorship
+        </h3>
+        <div className="flex flex-col">
+          <Link
+            href="https://dashboard.stripe.com/"
+            target="_blank"
+            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
+          >
+            <BiLogoStripe className="text-skin-icon-base text-xl" />
+            Stripe Dashboard
+          </Link>
+          <Link
+            href="https://foundation.gunjo.org/help/commerce-disclosure"
+            target="_blank"
+            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
+          >
+            <TbScale className="text-skin-icon-base text-xl" />
+            Commerce Disclosure
+          </Link>
+        </div>
+      </section>
+      <section>
+        <h3 className="text-skin-base mx-3 mb-2 text-xl font-semibold md:mx-0">
           Learn More
         </h3>
         <div className="flex flex-col">
@@ -178,29 +201,6 @@ export default async function SettingsContainer() {
           >
             <BsFillInfoCircleFill className="text-skin-icon-base text-xl" />
             About
-          </Link>
-        </div>
-      </section>
-      <section>
-        <h3 className="text-skin-base mx-3 mb-2 text-xl font-semibold md:mx-0">
-          Become a Sponsor
-        </h3>
-        <div className="flex flex-col">
-          <Link
-            href="https://donate.stripe.com/"
-            target="_blank"
-            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
-          >
-            <BiLogoStripe className="text-skin-icon-base text-xl" />
-            Manage Sponsorship
-          </Link>
-          <Link
-            href="https://foundation.gunjo.org/help/commerce-disclosure"
-            target="_blank"
-            className="border-skin-base text-skin-base hover:bg-skin-secondary flex items-center gap-2 border border-x-0 p-3 last:border-b md:border-x md:first:rounded-t-2xl md:last:rounded-b-2xl odd:[&:not(:last-child)]:border-b-0 even:[&:not(:last-child)]:border-b-0"
-          >
-            <TbScale className="text-skin-icon-base text-xl" />
-            Commerce Disclosure
           </Link>
         </div>
       </section>
