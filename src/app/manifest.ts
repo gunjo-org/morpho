df2bda9a611adbcc1c2ccfff66271d112792c785
Hitoki Horie <hitoki.horie@kikuzukikai.org>
2025-02-10 16:49:51 +0900

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     "name": "Gunjo",
     "short_name": "Gunjo",
     "description": "Bluesky client for the web",
-    "start_url": "/login",
+    "start_url": "/home",
     "display": "standalone",
     "background_color": "#fff",
     "icons": [
